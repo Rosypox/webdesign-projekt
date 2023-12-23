00b27d94a10c482e1e23fc0f8e7d88a45d337f38
Rosypox <koszalik10@gmail.com>
2023-12-23 20:27:52 +0100

    added header, image on landing page, scroll and buttons

--- code/webdesign.js
+++ code/webdesign.js
@@ -1,1 +1,20 @@
 //kod JavaScript
+const elements = document.querySelectorAll("#logo");
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.4,
+};
+
+const callbacks = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+};
+let observer = new IntersectionObserver(callbacks, options);
+
+elements.forEach((element) => {
+  observer.observe(element);
+});
