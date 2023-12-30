649a7fc74aefcbee733048d7983422a2bf7bd46b
Rosypox <koszalik10@gmail.com>
2023-12-30 17:10:58 +0100

    created portfolio page and calculator interface

---
+++ code/Contact Page/contact.js
@@ -0,0 +1,17 @@
+function unhideDiv() {
+  // Get the hidden div element
+
+  var hiddenDiv = document.getElementById("alert-message");
+  var hiddenDiv2 = document.getElementById("warning-message");
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var box = document.getElementById("box").checked;
+
+  if (name && email && message && box) {
+    hiddenDiv.style.display = "block";
+  } else {
+    // Change the display property to 'block' to make it visible
+    hiddenDiv2.style.display = "block";
+  }
+}
