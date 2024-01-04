633e4bd90794c2a949c8bbdd020da7179e2400e6
Adam <akoszalik@edu.cdv.pl>
2024-01-04 10:48:52 +0100

    completed project and made mobile version responsive

--- code/pricing page/pricing.js
+++ code/pricing page/pricing.js
@@ -1,5 +1,8 @@
 let sum = 0;
-var clicks = 0;
+var clicksRender = 0;
+var clicksAnimation = 0;
+var clicksPost = 0;
+var clicksHard = 0;
 //functions and values that get declared on page load
 window.onload = function () {
   var addBox = document.getElementById("add-render");
@@ -7,6 +10,7 @@ window.onload = function () {
   var addPost = document.getElementById("add-post");
   var addHardSurface = document.getElementById("add-hard-surface");
   var reset = document.getElementById("reset");
+  
   addBox.addEventListener("click", handler); //event listener for adding renders
   addAnimation.addEventListener("click", handler2); //event listener for adding animations
   addPost.addEventListener("click", handler3); //event listener for adding posts
@@ -14,19 +18,27 @@ window.onload = function () {
   //function for reseting all values in the calculator
   reset.addEventListener("click", function () {
     sum = 0;
-    clicks = 0;
+    clicksRender = 0;
+    clicksAnimation = 0;
+    clicksPost = 0;
+    clicksHard = 0;
     var result = document.getElementById("price-result");
     var ammount = document.getElementById("ammount");
     var ammountAnimations = document.getElementById("ammount-animations");
     var ammountPosts = document.getElementById("ammount-posts");
     var ammountHard = document.getElementById("ammount-hard-surface");
+  
     result.innerHTML = "final price...";
     ammount.innerHTML = "render";
     ammountAnimations.innerHTML = "animation";
-    ammountPosts.innerHTML = "post";
-    ammountHard.innerHTML = "hard-surface";
+    ammountPosts.innerHTML = "post + render";
+    ammountHard.innerHTML = "hard-surface";   
   });
+
+  
+ 
 };
+
 //function for adding renders
 function handler() {
   var result = document.getElementById("price-result");
@@ -36,13 +48,13 @@ function handler() {
   if (corrections > 0) {
     sum += 300 + corrections * 100;
     result.innerHTML = sum;
-    ammount.innerHTML = `${clicks} x renders`;
+    ammount.innerHTML = `${clicksRender} x renders`;
   } else {
     sum += 300;
     result.innerHTML = sum;
-    ammount.innerHTML = `${clicks} x renders`;
+    ammount.innerHTML = `${clicksRender} x renders`;
   }
-  if (clicks % 8 == 0) {
+  if (clicksRender % 8 == 0) {
     sum -= 1200;
     result.innerHTML = sum;
   }
@@ -52,15 +64,16 @@ function handler2() {
   var result = document.getElementById("price-result");
   var ammountAnimations = document.getElementById("ammount-animations");
   var corrections = document.getElementById("animation-corrections").value;
+  var length = document.getElementById("length-input").value;
   //if statement for checking if any bonus corrections were added
-  if (corrections > 0) {
-    sum += 4000 + corrections * 200;
+  if (corrections > 0 ||  length > 0) {
+    sum += 4000 + corrections * 200 + length * 130;
     result.innerHTML = sum;
-    ammountAnimations.innerHTML = `${clicks} x animations`;
-  } else {
+    ammountAnimations.innerHTML = `${clicksAnimation} x animations`;
+  } else  {
     sum += 4000;
     result.innerHTML = sum;
-    ammountAnimations.innerHTML = `${clicks} x animations`;
+    ammountAnimations.innerHTML = `${clicksAnimation} x animations`;
   }
 }
 //function for adding posts
@@ -72,11 +85,11 @@ function handler3() {
   if (corrections > 0) {
     sum += 500 + corrections * 100;
     result.innerHTML = sum;
-    ammountPosts.innerHTML = `${clicks} x posts`;
+    ammountPosts.innerHTML = `${clicksPost} x posts`;
   } else {
     sum += 500;
     result.innerHTML = sum;
-    ammountPosts.innerHTML = `${clicks} x posts`;
+    ammountPosts.innerHTML = `${clicksPost} x posts`;
   }
 }
 //function for adding hard-surface
@@ -85,10 +98,12 @@ function handler4() {
   var corrections = document.getElementById("hard-surface-corrections").value;
   var ammountHard = document.getElementById("ammount-hard-surface");
   var select = document.getElementById("type").value;
+  var hiddenDiv = document.getElementById("alert-message"); //alert message for when no type is selected
+ 
   //adds vehicle to sum
   switch (select) {
     case "vehicle":
-      ammountHard.innerHTML = `${clicks} x hard-surface`;
+      ammountHard.innerHTML = `${clicksHard} x hard-surface`;
       if ((select = "vehicle" && corrections > 0)) {
         sum += 1200 + corrections * 100;
         result.innerHTML = sum;
@@ -99,7 +114,7 @@ function handler4() {
       break;
     //adds building to sum
     case "building":
-      ammountHard.innerHTML = `${clicks} x hard-surface`;
+      ammountHard.innerHTML = `${clicksHard} x hard-surface`;
       if ((select = "building" && corrections > 0)) {
         sum += 1100 + corrections * 100;
         result.innerHTML = sum;
@@ -110,7 +125,7 @@ function handler4() {
       break;
     //adds weapon to sum
     case "weapon":
-      ammountHard.innerHTML = `${clicks} x hard-surface`;
+      ammountHard.innerHTML = `${clicksHard} x hard-surface`;
       if ((select = "vehicle" && corrections > 0)) {
         sum += 900 + corrections * 100;
         result.innerHTML = sum;
@@ -121,7 +136,7 @@ function handler4() {
       break;
     //adds prop to sum
     case "prop":
-      ammountHard.innerHTML = `${clicks} x hard-surface`;
+      ammountHard.innerHTML = `${clicksHard} x hard-surface`;
       if ((select = "prop" && corrections > 0)) {
         sum += 700 + corrections * 100;
         result.innerHTML = sum;
@@ -131,7 +146,14 @@ function handler4() {
       }
       break;
     default:
-      clicks--; //if no type is selected, the click is not counted
-      alert("Please select a type");
+      clicksHard--; //if no type is selected, the click is not counted
+          hiddenDiv.style.display = "block";
+        
+        
+  
   }
 }
+
+
+
+  
