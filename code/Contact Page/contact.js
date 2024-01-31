6ab17b4b3648705cadc4cd1e3def26be08ba03dd
Adam <akoszalik@edu.cdv.pl>
2024-01-31 09:59:46 +0100

    I forgot what I did here

--- code/Contact Page/contact.js
+++ code/Contact Page/contact.js
@@ -4,11 +4,12 @@ function unhideDiv() {
   var hiddenDiv = document.getElementById("alert-message");
   var hiddenDiv2 = document.getElementById("warning-message");
   var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
+  //var email = document.getElementById("email").value;
+  var pattern = elem.getAttribute("pattern"); // elem is reference to input element in this case
   var message = document.getElementById("message").value;
   var box = document.getElementById("box").checked;
 
-  if (name && email && message && box) {
+  if (name && pattern && message && box) {
     hiddenDiv.style.display = "block";
     hiddenDiv2.style.display = "none";
   } else {
